7197c92f2d313114c6a40481366d871c2531cbee
Transcendence <109620661+Trancendence@users.noreply.github.com>
2022-12-08 16:30:38 +0200

    task 1
    task 1

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+console.log(`Number of categories: ${document.getElementById('categories').getElementsByTagName('h2').length}`)
+console.log("Category: Animals");
+console.log(`Elements: ${document.getElementById('animals').getElementsByTagName('li').length}`);
+console.log("Category: Products");
+console.log(`Elements: ${document.getElementById('item').getElementsByTagName('li').length}`);
+console.log("Category: Technologies");
+console.log(`Elements: ${document.getElementById('technologies').getElementsByTagName('li').length}`);
+
+// Task 1
