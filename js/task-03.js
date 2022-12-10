d9ccbcff10ab43706c74bc77c157683a5d34abef
Transcendence <baydindenys@ukr.net>
2022-12-10 17:09:44 +0200

    task 3

--- js/task-03.js
+++ js/task-03.js
@@ -13,14 +13,10 @@ const images = [
   },
 ];
 
-
-const photo = document.querySelector(".gallery");
-photo.insertAdjacentHTML("afterbegin", `<img class="image" width="400" height="300"
-src="https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" 
-alt="White and Black Long Fur Cat"></img>
-<img class="image" width="400" height="300"
-src="https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" 
-alt="Orange and White Koi Fish Near Yellow Koi Fish"></img>
-<img class="image" width="400" height="300"
-src="https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" 
-alt="Group of Horses Running"></img>`);
+const ulEl = document.querySelector('.gallery');
+images.forEach(el => {
+  ulEl.insertAdjacentHTML(
+    'afterbegin',
+    `<li><img src = "${el.url}" alt = "${el.alt}"  width = "300" height = "200" /></li>`,
+  );
+});
