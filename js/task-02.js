442b1512d5fbc358ef415607eab6f4acd10f9067
Transcendence <baydindenys@ukr.net>
2022-12-10 17:12:27 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -8,11 +8,12 @@ const ingredients = [
 ];
 
 
-const list = document.querySelector("#ingredients");
-const ingridient = document.createElement(`li`);
-ingridient.textContent = ingredients;
-ingridient.classList.add("item");
-list.append(ingridient);
+// const list = document.querySelector("#ingredients");
+// const ingridient = document.createElement(`li`);
+// ingridient.textContent = ingredients;
+// ingridient.classList.add("item");
+// list.append(ingridient);
 
-
-
+const ulEl = document.querySelector('#ingredients');
+const list = ingredients.reduce((str, item) => str + `<li>${item}</li>`, '');
+ulEl.innerHTML = list;
