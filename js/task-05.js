0bac2182f381286a160981ce1b0b14f6542b2f70
Transcendence <baydindenys@ukr.net>
2022-12-10 16:58:20 +0200

    task 5

--- js/task-05.js
+++ js/task-05.js
@@ -1,2 +1,10 @@
-const text = document.querySelector(`input#name-input`);
-text.textContent()
+let input = document.getElementById("name-input");
+let nameOutput = document.getElementById("name-output");
+
+input.oninput = function () {
+    if (input.value === '') {
+       nameOutput.innerHTML = 'незнакомец';
+    } else{
+    nameOutput.innerHTML = input.value;
+    }
+}
