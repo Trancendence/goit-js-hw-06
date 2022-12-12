c73a3536479115c86bbc1719463d89931dab4abe
Transcendence <baydindenys@ukr.net>
2022-12-12 23:20:21 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -7,22 +7,14 @@ const ingredients = [
   'Condiments',
 ];
 
+const ingridientList = document.querySelector('#ingredients')
+const liArray = []
 
-// const ulEl = document.querySelector('#ingredients');
-// const list = ingredients.reduce((str, item) => str + `<li>${item}</li>`, '');
-// ulEl.innerHTML = list;
+ingredients.forEach(ingredient => {
+	const item = document.createElement('li')
+	item.className = 'item'
+	item.textContent = ingredient
+	liArray.push(item)
+})
 
-
-const list = arrayOfItems => {
-
-  ingredientsRef.append(
-    ...arrayOfItems.map(itemName => {
-      const li = document.createElement('li');
-      li.textContent = itemName;
-      return li;
-    }),
-  );
-};
-const ingredientsRef = document.querySelector('ul#ingredients');
-
-list(ingredients);
+ingridientList.append(...liArray)
